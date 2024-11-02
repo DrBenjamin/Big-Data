ddcd71f9eee373c9f1fb9f628c424c1369d84c12
Benjamin Gross <ben@benbox.org>
2024-11-02 08:55:17 +0100

    added python pyspark

--- Python.py
+++ Python.py
@@ -1,4 +1,42 @@
 # Python script
-# Show Python Version
+import streamlit as st
+# Import the SparkSession module
+from pyspark.sql import SparkSession
 import sys
-print(sys.version)
+import requests
+
+# Streamlit Title
+st.title("Streamlit with PySpark")
+
+# Show Python Version
+st.write(sys.version)
+
+# Create a SparkSession
+spark = SparkSession.builder.config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider").getOrCreate()
+st.success("SparkSession created successfully")
+
+# Get the SparkContext from the SparkSession
+sc = spark.sparkContext
+st.success("SparkContext retreived successfully")
+
+# Set the MinIO access key, secret key, endpoint, and other configurations
+sc._jsc.hadoopConfiguration().set("fs.s3a.access.key", "HEALTH")
+sc._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "NOentry#23")
+sc._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "https://localhost:9000")
+sc._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")
+sc._jsc.hadoopConfiguration().set("fs.s3a.connection.ssl.enabled", "false")
+sc._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+sc._jsc.hadoopConfiguration().set("fs.s3a.connection.ssl.enabled", "false")
+st.success("MinIO configurations set successfully")
+
+# Read a JSON file from an MinIO bucket using the access key, secret key, 
+# and endpoint configured above
+#url = "https://localhost:9000/templategenerator/file.json"
+#response = requests.get(url)
+#with open("/tmp/file.json", "wb") as f:
+#    f.write(response.content)
+df = spark.read.option("header", "false").json(f"s3a://templategenerator/file.json")
+st.success("Data loaded successfully")
+    
+# Show data
+st.dataframe(df, selection_mode='multi-column')
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Big-Data
 
 Big Data course
+
+## MinIO
+
+To create `access_key` and `secret_key`, use the MinIO console
+
+```bash
+
+```
