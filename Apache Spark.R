6579c298df2c18f0b39cee3c1c5681b7a80bb722
Benjamin Gross <ben@benbox.org>
2024-11-02 12:15:21 +0100

    cleaned up

--- Apache Spark.R
+++ Apache Spark.R
@@ -3,10 +3,33 @@
 # docker run -it --rm spark:r /opt/spark/bin/sparkR
 install.packages("sparklyr")
 library(sparklyr)
+library(devtools)
+library(httr)
+install_github("nagdevAmruthnath/minio.s3")
+library("minio.s3")
+library(aws.s3)
+library(tidyverse)
+library(dplyr)
+library(readr)
 
 # Local Spark Cluster
 sparklyr::spark_install(version = "3.5.0")
 sc <- spark_connect(master = "local")
 
-# Spark Dataframe
+# Setting the environment variables
+# https://localhost:9001/browser/templategenerator
+set_config(config(ssl_verifyhost = 0L, ssl_verifypeer = 0L))
+Sys.setenv("AWS_ACCESS_KEY_ID" = "health",
+           "AWS_SECRET_ACCESS_KEY" = "NOentry#23",
+           "AWS_SSL_ENABLED" = "TRUE",
+           "AWS_S3_ENDPOINT" = "127.0.0.1:9000")
+
+# Get file from the MinIO server
+b <- get_bucket(bucket = 'templategenerator', region = "", use_https = TRUE)
+iris <- aws.s3::s3read_using(FUN = read.csv, object = "iris.csv", bucket = b, opts = list(use_https = TRUE, region = ""))
+
+# Spark dataframe
 iris_tbl <- copy_to(sc, iris)
+
+# Show dataframe
+iris_tbl
---
+++ Apache Spark.py
@@ -0,0 +1,68 @@
+# Import the SparkSession module
+from pyspark.sql import SparkSession
+import streamlit as st
+import pandas as pd
+import sys
+import os
+import io
+import urllib3
+from minio import Minio
+
+# Streamlit Title
+st.title("Streamlit with PySpark")
+
+# Show Python Version
+st.write(sys.version)
+
+# Create a SparkSession
+spark = SparkSession.builder.appName("SparkApp").master("local").config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider").getOrCreate()
+st.success("SparkSession created successfully")
+
+# Get the SparkContext from the SparkSession
+sc = spark.sparkContext
+st.success("SparkContext retreived successfully")
+
+# Disable SSL verification warnings
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Setting the environment variables
+os.environ["AWS_ACCESS_KEY_ID"] = "health"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "NOentry#23"
+os.environ["AWS_SSL_ENABLED"] = "TRUE"
+os.environ["AWS_S3_ENDPOINT"] = "127.0.0.1:9000"
+
+# Initialize Minio client
+client = Minio(
+    endpoint=os.environ["AWS_S3_ENDPOINT"],
+    access_key=os.environ["AWS_ACCESS_KEY_ID"],
+    secret_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+    secure=True,
+    http_client=urllib3.PoolManager(cert_reqs='CERT_NONE')
+)
+
+# Get files from the MinIO server
+bucket_name = 'templategenerator'
+
+# Read the CSV file into a DataFrame
+iris_csv = client.get_object('templategenerator', "iris.csv")
+st.success("Data loaded successfully")
+
+# Load the iris dataset into a Pandas DataFrame
+iris_df = pd.read_csv(io.BytesIO(iris_csv.read()))
+
+# Convert the Pandas DataFrame to a Spark DataFrame
+iris_spark_df = spark.createDataFrame(iris_df)
+    
+# Show data
+st.dataframe(iris_spark_df, selection_mode='multi-column')
+
+
+
+
+
+
+
+
+# Show the Spark DataFrame
+iris_spark_df.show()
+
